7168345ef84763f6a2da8b6319dc496dd9a7512b
shhdh <90132007+ShahidHameed4@users.noreply.github.com>
2024-03-31 00:13:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify
 import joblib
 
@@ -5,20 +6,12 @@ app = Flask(__name__)
 
 model = joblib.load('iris_classifier.pkl')
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    features = [
-        data['sepal_length'],
-        data['sepal_width'],
-        data['petal_length'],
-        data['petal_width'],
-        data['PetalLengthCm']
-    ]
+    features = [data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width'],data['PetalLengthCm']]
     prediction = model.predict([features])[0]
     return jsonify({'prediction': prediction})
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
